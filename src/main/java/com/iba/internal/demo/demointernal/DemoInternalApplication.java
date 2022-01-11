85e39bf8131d524f29136e2a59f6544b3373ff76
Denis Strazhevich <dstrazhevich@ibagroup.eu>
2022-01-11 14:41:15 +0300

    Initial commit

---
+++ src/main/java/com/iba/internal/demo/demointernal/DemoInternalApplication.java
@@ -0,0 +1,13 @@
+package com.iba.internal.demo.demointernal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInternalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoInternalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iba/internal/demo/demointernal/DemoInternalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iba.internal.demo.demointernal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoInternalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
